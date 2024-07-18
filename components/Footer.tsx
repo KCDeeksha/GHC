ef1c598141767afe81c4cd2e76dc9061d8d2f6c1
KCDeeksha <deeksha.chandru@gmail.com>
2024-07-18 13:40:58 +0530

    second commit

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,28 @@
 import React from "react";
+import Link from "next/link";
 
 const Footer = () => {
     return (
-    <div className="bg-[#212121] py-8 mt-32">
-    <div className="container text-center text-white">
-        <p>&copy; Copyright GHC | All Right Reserved.</p>
-    </div>
-    </div>
+        <div className="bg-white py-4 sticky bottom-0 w-full">
+            <div className="container flex justify-between items-center mx-auto px-8">
+                <Link href="/" className='flex flex-col items-center'>
+                    <img src='/fh.png' alt='Home' className='w-10 h-10' />
+                    <span className="text-xs"></span>
+                </Link>
+                <Link href="/track" className='flex flex-col items-center'>
+                    <img src='/track.png' alt='Track' className='w-10 h-10' />
+                    <span className="text-xs"></span>
+                </Link>
+                <Link href="/search" className='flex flex-col items-center'>
+                    <img src='/search.png' alt='Search' className='w-10 h-10' />
+                    <span className="text-xs"></span>
+                </Link>
+                <Link href="/account" className='flex flex-col items-center'>
+                    <img src='/account.png' alt='Account' className='w-10 h-10' />
+                    <span className="text-xs"></span>
+                </Link>
+            </div>
+        </div>
     )
 }
 
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,43 +1,56 @@
 import Link from 'next/link';
 import React from 'react';
 import { RxHamburgerMenu } from "react-icons/rx";
-import {AiOutlineShoppingCart} from "react-icons/ai"
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useAppSelector } from '@/redux/hooks';
 
-const NavBar = ({setShowCart}: any) => {
-
-    const cartCount = useAppSelector((state) => state.cartReducer.length);
-
-  return (
-    <div className='bg-white py-4 sticky top-0 z-10'>
-        <div className='container flex justify-between items-center'>
-            <RxHamburgerMenu className="sm:hidden text-[26px]" />
-            <Link href="/" className='text-4xl font-semibold hover:text-accent'>
-                Logo
-            </Link> 
-
-        <ul className='gap-6 hidden sm:flex'>
-            <Link href="/" className='navLink'>
-                Home
-            </Link>
-            <li className='navLink'>Shop</li>
-            <li className='navLink'>Blog</li>
-            <li className='navLink'>Pages</li>
-            <li className='navLink'>Contact</li>
-        </ul>
-        <div className='flex gap-6 text-[26px]'>
-            <div className='relative cursor-pointer'
-            onClick={() => setShowCart(true)}>
-                <AiOutlineShoppingCart />
-                <div className='absolute top-[-15px] right-[-10px] bg-red-600 w-[25px] h-[25px] rounded-full text-white text-[14px] grid place-items-center'>
-                    {cartCount}
-                </div>
-            </div>
-        </div>
-        </div>
-      
-    </div>
-  )
+interface NavBarProps {
+    setShowCart: (show: boolean) => void;
 }
 
-export default NavBar
+const NavBar: React.FC<NavBarProps> = ({ setShowCart }) => {
+    const cartCount = useAppSelector((state) => state.cartReducer.length);
+
+    return (
+        <div className='bg-white py-4'>
+            <div className='container flex flex-col items-center'>
+                <div className='w-full flex justify-between items-center mb-8 sticky top-0 z-10 bg-white py-4'>
+                    <div className='flex items-center w-full justify-between'>
+                        <RxHamburgerMenu className="text-[26px]" />
+                        <Link href="/" className='text-4xl font-semibold hover:text-accent mx-auto'>
+                            <img src='/logo.png' className='h-12' />
+                        </Link>
+                        <div className='flex gap-6 text-[26px]'>
+                            <div className='relative cursor-pointer' onClick={() => setShowCart(true)}>
+                                <AiOutlineShoppingCart />
+                                <div className='absolute top-[-15px] right-[-10px] bg-red-600 w-[25px] h-[25px] rounded-full text-white text-[14px] grid place-items-center'>
+                                    {cartCount}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <ul className='flex gap-6 sm:flex'>
+                    <Link href="/" className='navLink'>
+                        <img src='/Group19566.png' alt='Home' className='w-24 h-24' />
+                    </Link>
+                    <Link href="/shop" className='navLink'>
+                        <img src='/Group19564.png' alt='Shop' className='w-24 h-24' />
+                    </Link>
+                    <Link href="/blog" className='navLink'>
+                        <img src='/Group19569.png' alt='Blog' className='w-24 h-24' />
+                    </Link>
+                    <li className='navLink relative w-24 h-24'>
+                        <img src='/blogbg.png' alt='Blog Background' className='absolute inset-0 w-full h-full' />
+                        <img src='/blog.png' alt='Blog Overlay' className='absolute inset-0 w-full h-full transform -translate-y-4' />
+                    </li>
+                    {/* <Link href="/contact" className='navLink'>
+                        <img src='/Group19564.png' alt='Contact' className='w-24 h-24' />
+                    </Link> */}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
